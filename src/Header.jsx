dec4681a0182e7bcfe74f64ccbf6ebd6a6a7b375
WillDang14 <will.dang1402@gmail.com>
2025-04-22 10:33:10 -0400

    week-05 with conditional rendering

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,20 +4,26 @@ import ctdLogo from './assets/icons/mono-blue-logo.svg';
 import shoppingCart from './assets/icons/shoppingCart.svg';
 
 /* ========================================================= */
-function Header({ cart }) {
-  useEffect(() => {
-    console.log('cart', cart); // đặt chỗ này có vẻ ổn
+function Header({ cart, handleOpenCart }) {
+  // Week 5 ask to remove useEffect()
+  // useEffect(() => {
+  //   console.log('cart', cart); // đặt chỗ này có vẻ ổn
 
-    cart.forEach((item) => {
-      console.log(item.baseName, item.cartItemId);
-    });
+  //   cart.forEach((item) => {
+  //     console.log(item.baseName, item.cartItemId);
+  //   });
 
-    if (cart.length > 0) {
-      console.log('--end of cart--');
-    }
-  });
+  //   if (cart.length > 0) {
+  //     console.log('--end of cart--');
+  //   }
+  // });
 
   // console.log('cart', cart); // chỗ này ko được
+
+  // week 5 ==>> đém số lượng item trong cart
+  function getItemCount() {
+    return cart.reduce((acc, item) => acc + item.itemCount, 0);
+  }
 
   return (
     <header>
@@ -28,10 +34,12 @@ function Header({ cart }) {
       </div>
 
       <div className="shoppingCart">
-        <button type="button">
+        <button type="button" onClick={handleOpenCart}>
           <img src={shoppingCart} alt="" />
 
-          <p className="cartCount">{cart.length}</p>
+          {/* <p className="cartCount">{cart.length}</p> */}
+
+          <p className="cartCount">{getItemCount()}</p>
         </button>
       </div>
     </header>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
 import './index.css';
+
+import './assets/fonts/WorkSans/WorkSans-Regular.ttf';
+import './assets/fonts/WorkSans/WorkSans-ExtraBold.ttf';
+import './assets/fonts/WorkSans/WorkSans-Light.ttf';
 /* ========================================================= */
 
 createRoot(document.getElementById('root')).render(
